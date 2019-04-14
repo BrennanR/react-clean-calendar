e70ffcc4d6f3634d67d249c72784358554ccc00c
Brennan Rauert <brennanrauert@gmail.com>
2019-04-14 15:26:40 -0600

    Pager is working.

--- src/examples/Example8.js
+++ src/examples/Example8.js
@@ -1,43 +1,49 @@
 // @flow
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState, type Element } from 'react';
 import { animated, useSprings } from 'react-spring';
 import { useMeasure } from '@softbind/react-hooks';
 
-const Pager = ({ currentPageIndex, pages }: { currentPageIndex: number, pages: Array<any> }) => {
+import { randomColor, invertColor, rgbToHex } from './colorUtils';
+
+type Page = {
+  key: string,
+  element: Element<*>,
+};
+
+const xOffsetForPage = (pageIndex, currentPageIndex, width) => {
+  const offsetDelta = pageIndex - currentPageIndex;
+  const pageOffset = offsetDelta * width;
+  return pageOffset;
+};
+
+const pageIndex = (pages, currentPageKey) => pages.findIndex(page => page.key === currentPageKey);
+
+const Pager = ({ currentPageKey, pages }: { currentPageKey: string, pages: Array<Page> }) => {
   const ref = useRef(null);
   const { bounds: { width = -1 } = {} } = useMeasure(ref, 'bounds');
-  const [internalPageIndex, setInternalPageIndex] = useState(currentPageIndex);
-
-  const xOffset = pageIndex => {
-    const pageOffset = (pageIndex - currentPageIndex) * width;
-    return pageOffset;
-  };
-
-  // Initialize the springSet x-positions to 0. Since we need to wait for a measurement, we don't know the correct
-  // values at initialization time.
+  // Set the spring x-positions to 0. We need to wait for useMeasure, we don't know the correct values initially.
   const [springs, springsSet] = useSprings(pages.length, () => ({ x: 0 }));
 
-  // This effect handles animating the page x positions when the currentPageIndex changes.
-  useEffect(() => {
-    console.log({ currentPageIndex, internalPageIndex });
-    if (currentPageIndex !== internalPageIndex) {
-      const diff = currentPageIndex === 0 ? 0 : internalPageIndex - currentPageIndex + 1;
+  useLayoutEffect(() => {
+    springsSet(index => {
+      return { x: xOffsetForPage(index, pageIndex(pages, currentPageKey), width), immediate: true };
+    });
+  }, [pages]);
 
-      springsSet(index => {
-        const newXOffset = xOffset(index + diff);
-        // console.log({ index, newXOffset });
-        return { x: newXOffset, immediate: false };
-      });
-      setInternalPageIndex(currentPageIndex);
-    }
-  }, [currentPageIndex, internalPageIndex]);
-
-  // This effect handles initially placing the pages. If the width changes, that placement changs.
+  // This effect handles animating the page x-positions when the currentPageIndex changes.
   useEffect(() => {
     springsSet(index => {
-      const initialXOffset = xOffset(index);
-      // console.log({ index, initialXOffset });
+      return { x: xOffsetForPage(index, pageIndex(pages, currentPageKey), width), immediate: false };
+    });
+  }, [currentPageKey]);
+
+  // This effect handles initially placing the pages. If the width changes, that placement changes.
+  useEffect(() => {
+    springsSet(index => {
+      const initialXOffset = xOffsetForPage(index, pageIndex(pages, currentPageKey), width);
+      // console.log({ index, initialXOffset, pageIndex: pageIndex() });
+      // As the window resizes immediately update the x-coord, don't animate it.
       return { x: initialXOffset, immediate: true };
     });
   }, [width]);
@@ -57,36 +63,71 @@ const Pager = ({ currentPageIndex, pages }: { currentPageIndex: number, pages: A
     >
       {springs.map(({ x }, i) => (
         <animated.div
-          key={i}
+          key={pages[i].key}
           style={{ position: `absolute`, transform: x.interpolate(x => `translate3d(${x}px,0,0)`) }}
         >
-          {pages[i]}
+          {pages[i].element}
         </animated.div>
       ))}
     </div>
   );
 };
 
+const pageForKey = (key: string) => {
+  const bgColor = randomColor();
+  const fgColor = invertColor(rgbToHex(bgColor), true);
+  return {
+    key: `pg${key}`,
+    element: (
+      <div
+        style={{
+          display: `flex`,
+          height: 100,
+          width: 100,
+          backgroundColor: bgColor,
+          alignItems: `center`,
+          justifyContent: `center`,
+        }}
+      >
+        <p style={{ fontSize: `1.3em`, fontWeight: `bold`, color: fgColor }}>{key}</p>
+      </div>
+    ),
+  };
+};
+
 export const Example8 = () => {
-  const [currentPageIndex, setCurrentPageIndex] = useState(1);
+  const [currentPage, setCurrentPage] = useState(101);
+  const [pages, setPages] = useState([pageForKey(100), pageForKey(101), pageForKey(102)]);
+  const [minKey, setMinKey] = useState(100);
+  const [maxKey, setMaxKey] = useState(102);
 
-  console.log({ currentPageIndex });
+  const appendPages = () => {
+    setPages(prevPages => [
+      ...prevPages,
+      pageForKey((maxKey + 1).toString()),
+      pageForKey((maxKey + 2).toString()),
+      pageForKey((maxKey + 3).toString()),
+    ]);
+    setMaxKey(prevMaxPage => prevMaxPage + 3);
+  };
 
-  const pages = [
-    <div key="pg1" style={{ height: 100, width: 100, backgroundColor: `red` }} />,
-    <div key="pg2" style={{ height: 100, width: 100, backgroundColor: `green` }} />,
-    <div key="pg3" style={{ height: 100, width: 100, backgroundColor: `blue` }} />,
-  ];
+  const prependPages = () => {
+    setPages(prevPages => [
+      pageForKey((minKey - 3).toString()),
+      pageForKey((minKey - 2).toString()),
+      pageForKey((minKey - 1).toString()),
+      ...prevPages,
+    ]);
+    setMinKey(prevMinPage => prevMinPage - 3);
+  };
 
   return (
     <div style={{ flex: 1, width: `100%`, alignItems: `center`, justifyContent: `center` }}>
-      <button onClick={() => setCurrentPageIndex(prevIndex => (prevIndex === 0 ? pages.length - 1 : prevIndex - 1))}>
-        Left
-      </button>
-      <button onClick={() => setCurrentPageIndex(prevIndex => (prevIndex === pages.length - 1 ? 0 : prevIndex + 1))}>
-        Right
-      </button>
-      <Pager currentPageIndex={currentPageIndex} pages={pages} />
+      <button onClick={prependPages}>Prepend Pages</button>
+      <button onClick={() => setCurrentPage(prevKey => (prevKey === minKey ? maxKey : prevKey - 1))}>Left</button>
+      <button onClick={() => setCurrentPage(prevKey => (prevKey === maxKey ? minKey : prevKey + 1))}>Right</button>
+      <button onClick={appendPages}>Append Pages</button>
+      <Pager currentPageKey={`pg${currentPage}`} pages={pages} />
     </div>
   );
 };
